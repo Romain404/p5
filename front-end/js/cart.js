8a0ed0663b9a547f97f3c3548e63f021544eb1a4
Romain Ducouret <romainducouret@air-de-romain.home>
2021-07-16 22:07:04 +0200

    Updating cart.js files

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,4 +1,3 @@
-const productAmount = document.getElementById("productAmount");
 const price = document.getElementById("price");
 const priceTotal = document.getElementById("priceTotal");
 const lens = document.getElementById("lens");
@@ -7,14 +6,10 @@ const error = document.getElementById("error");
 const table = document.querySelector("tbody");
 const cartTitle = document.getElementById("cart-title");
 
-///////////////////////////// Ajout d'un article au panier ////////////////////////////
+///////////////////////////////////////// Ajout d'un article au panier /////////////////////////////////////
 
 var JSONdata = localStorage.getItem("cartContent");
 var data = JSON.parse(JSONdata);
-
-price.textContent = data.price;
-name.textContent = data.name;
-lens.textContent = data.lense;
 
 const numberOfArticle = data.length;
 
@@ -22,36 +17,58 @@ for (let i = 0; i < numberOfArticle; i++) {
   let newItem = document.createElement("tr");
   newItem.id = "product";
   table.appendChild(newItem);
-  console.log(JSONdata.name);
   newItem.innerHTML = `
-  <tr id="product">
-    <td id="name">${data.name}</td>
-    <td id="lens">${data.lense}</td>
-    <td id="amountCell"><select id="productAmount"></select><button id="delete-btn"><i class="fas fa-trash-alt fa-2x"></i></button></td>
-    <td id="price">${data.price}</td>
+    <td id="name">${data[i].name}</td>
+    <td id="lens">${data[i].lense}</td>
+    <td id="amountCell">
+      <select id="productAmount"></select>
+      <button id="delete-btn">
+      <i class="fas fa-trash-alt fa-2x"></i>
+      </button>
+    </td>
+    <td id="price">${data[i].price}</td>
     <td id="priceTotal"></td>
-  </tr>
   `;
+
+  let productName = data[i].name;
+  console.log("0001 Nom : " + productName);
+  let productPrice = data[i].price;
+  console.log("0002 Prix : " + productPrice);
+  let productLense = data[i].lense;
+  console.log("0003 Lentille : " + productLense);
+
+  //////////////////////////////////////// Quantity Setting ////////////////////////////////////////////////
+
+  let maxAmount = 12;
+  let minAmount = 1;
+
+  for (let i = 0; i < maxAmount; i++) {
+    let select = document.getElementById("productAmount");
+    let number = document.createElement("option");
+    select.appendChild(number);
+    console.log(select[i]);
+    number.textContent = minAmount;
+    minAmount++;
+  }
 }
-/////////////////// Nombre d'article dans le panier ///////////////////
+
+function addingQuantity() {
+  let maxAmount = 12;
+  let minAmount = 1;
+
+  //  for (let i = 0; i < maxAmount; i++) {
+  //    let select = document.querySelectorAll("select");
+  //    let number = document.createElement("option");
+  //    number.textContent = minAmount;
+  //    select.appendChild(number);
+  //    minAmount++;
+  //  }
+}
+
+/////////////////////////////////////// Nombre d'article dans le panier ////////////////////////////////////
 cartTitle.textContent = "Votre panier ( " + data.length + " articles )";
 
-////////////////// Prix unitaire multiplié par le nombre d'article////////////
-function totalArticle() {
-  priceTotal.textContent = price.textContent * productAmount.value;
-}
+/////////////////////////////////////// Prix unitaire multiplié par le nombre d'article/////////////////////
+function totalArticle() {}
 
-totalArticle();
-
-/////////////////////////////// Sélecteur de quantité /////////////////////////////////////
-let maxAmount = 12;
-let minAmount = 1;
-
-for (let i = 0; i < maxAmount; i++) {
-  let number = document.createElement("option");
-  productAmount.appendChild(number);
-  number.textContent = minAmount;
-  minAmount++;
-}
-
-//////////////////////////// Suppression d'un produit /////////////////////////////////
+/////////////////////////////////////// Suppression des produits ///////////////////////////////////////////
